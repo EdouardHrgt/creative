4df9d15d0fbd009bf812c4a8615452d5471d0eb5
Edouard Herrengt <86005588+EdouardHrgt@users.noreply.github.com>
2023-10-07 01:20:46 +0200

    init

---
+++ src/views/HomePage.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <h1>HelloWorld!</h1>
+</template>
+
+<style scoped>
+h1 {
+  font-size: clamp(3rem, 4vw, 5rem);
+  color: hotpink;
+  text-align: center;
+}
+</style>
